12de3d1c1f6afc567dc128188789e2aff3040107
Magnus Lopez <ac148904@avcol.school.nz>
2024-03-13 12:31:50 +1300

    Coded minimum amount of questions for the level Easy. added comments

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -7,21 +7,29 @@
 
         static void Main(string[] args)
         {
+            // Storing typed string method inputName in a string variable.
             string nameRead = inputName();
             Console.Clear();
             Console.WriteLine($"Welcome, {nameRead}");
         }
         static String inputName()
         {
+            // string variable used to store the entered value for name.
             string nameEntered;
-            Console.WriteLine("Proceed with your name, please:");
+            Console.WriteLine("Proceed with your name, please :");
+
+            // input 
             nameEntered = Console.ReadLine();
             return nameEntered;
         }
 
         static String levelHierachy()
         {
-            string[] lvlEasy = { };
+            string[] lvlEasy = {"What is the number one in Te Reo?\nA) Tahi\nB) Kai\nC) Kakariki", // Q1
+                                "In Maori, which mean hello and goodbye? (In order)\nA) Kia Ora, Haere Ra\nB) Etu, Enoho\nC) Haere Ra, Ora Kia", // Q2
+                                "What is the word for family?\nA) Vin Diesel\nB) Whanau\nC) Whero" // Q3
+                               };
+
         }
     }
 }
