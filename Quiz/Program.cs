9aee9929ac43640bb8fdb7f74928ffa0047e1f7d
Magnus Lopez <ac148904@avcol.school.nz>
2024-03-12 14:16:47 +1300

    Here I added basic skeleton of the code for my quiz. I added a few methods.

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -2,9 +2,26 @@
 {
     internal class Program
     {
+
+        // Hello
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string nameRead = inputName();
+            Console.Clear();
+            Console.WriteLine($"Welcome, {nameRead}");
+        }
+        static String inputName()
+        {
+            string nameEntered;
+            Console.WriteLine("Proceed with your name, please:");
+            nameEntered = Console.ReadLine();
+            return nameEntered;
+        }
+
+        static String levelHierachy()
+        {
+            string[] lvlEasy = { };
         }
     }
 }
